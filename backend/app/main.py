b3f68ffc47531c5660715c56352e0dfc6148c742
Kritika Bansal <kritikabansal@Kritikas-MacBook-Pro.local>
2026-04-15 21:47:24 +0530

    fix railway settings error

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.auth import router as auth_router
-from app.routes.student import router as student_router
-from app.routes.teacher import router as teacher_router
-from app.routes.subjects import router as subjects_router
-from app.routes.timetable import router as timetable_router
 from app.routes.attendance import router as attendance_router
-from app.routes.qr import router as qr_router
+from app.routes.face_attendance import router as face_attendance_router
+from app.routes.qr_attendance import router as qr_router
+from app.routes.student import router as student_router
+from app.routes.subject import router as subject_router
+from app.routes.teacher import router as teacher_router
+from app.routes.timetable import router as timetable_router
 
 app = FastAPI(title="VisionGuard API")
 
@@ -27,9 +28,10 @@ def home():
     }
 
 app.include_router(auth_router)
-app.include_router(student_router)
-app.include_router(teacher_router)
-app.include_router(subjects_router)
-app.include_router(timetable_router)
 app.include_router(attendance_router)
+app.include_router(face_attendance_router)
 app.include_router(qr_router)
+app.include_router(student_router)
+app.include_router(subject_router)
+app.include_router(teacher_router)
+app.include_router(timetable_router)
